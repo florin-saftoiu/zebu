126e48913f3f12a98bb1c2072458045c03666af7
Florin Săftoiu <florin.saftoiu@soft-to-you.com>
2021-03-23 16:51:20 +0200

    di and ei

--- src/z80/cpu.rs
+++ src/z80/cpu.rs
@@ -17,7 +17,7 @@ const OPCODES: [(&str, fn(&mut Z80CPU, &mut dyn ReadWrite) -> u8, u8, u8); 256] 
 /* c0 */ ("RET NZ"    , Z80CPU::ret_nz     , 0, 5), ("POP BC"    , Z80CPU::pop_bc     , 0, 10), ("JP NZ,"     , Z80CPU::jp_nz_nn    , 2, 10), ("JP"         , Z80CPU::jp_nn         , 2, 10), ("CALL NZ,"  , Z80CPU::call_nz_nn    , 2, 10), ("PUSH BC"   , Z80CPU::push_bc       , 0, 11), ("???"       , Z80CPU::invalid_opcode, 0,  4), ("RST 00h"   , Z80CPU::rst_00h       , 0, 11), ("RET Z"     , Z80CPU::ret_z         , 0,  5), ("RET"       , Z80CPU::ret           , 0, 10), ("JP Z,"     , Z80CPU::jp_z_nn       , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0, 4), ("CALL Z," , Z80CPU::call_z_nn     , 2, 10), ("CALL"    , Z80CPU::call_nn       , 2, 17), ("???"       , Z80CPU::invalid_opcode, 0, 4), ("RST 08h" , Z80CPU::rst_08h       , 0, 11), /* c0 */
 /* d0 */ ("RET NC"    , Z80CPU::ret_nc     , 0, 5), ("POP DE"    , Z80CPU::pop_de     , 0, 10), ("JP NC,"     , Z80CPU::jp_nc_nn    , 2, 10), ("???"        , Z80CPU::invalid_opcode, 0,  4), ("CALL NC,"  , Z80CPU::call_nc_nn    , 2, 10), ("PUSH DE"   , Z80CPU::push_de       , 0, 11), ("???"       , Z80CPU::invalid_opcode, 0,  4), ("RST 10h"   , Z80CPU::rst_10h       , 0, 11), ("RET C"     , Z80CPU::ret_c         , 0,  5), ("???"       , Z80CPU::invalid_opcode, 0,  4), ("JP C,"     , Z80CPU::jp_c_nn       , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0, 4), ("CALL C," , Z80CPU::call_c_nn     , 2, 10), ("IX"      , Z80CPU::ix            , 0,  0), ("???"       , Z80CPU::invalid_opcode, 0, 4), ("RST 18h" , Z80CPU::rst_18h       , 0, 11), /* d0 */
 /* e0 */ ("RET PO"    , Z80CPU::ret_po     , 0, 5), ("POP HL"    , Z80CPU::pop_hl     , 0, 10), ("JP PO,"     , Z80CPU::jp_po_nn    , 2, 10), ("EX (SP), HL", Z80CPU::ex_ptr_sp_hl  , 0, 19), ("CALL PO,"  , Z80CPU::call_po_nn    , 2, 10), ("PUSH HL"   , Z80CPU::push_hl       , 0, 11), ("AND"       , Z80CPU::and_n         , 1,  7), ("RST 20h"   , Z80CPU::rst_20h       , 0, 11), ("RET PE"    , Z80CPU::ret_pe        , 0,  5), ("???"       , Z80CPU::invalid_opcode, 0,  4), ("JP PE,"    , Z80CPU::jp_pe_nn      , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0, 4), ("CALL PE,", Z80CPU::call_pe_nn    , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0,  4), ("XOR"       , Z80CPU::xor_n         , 1, 7), ("RST 28h" , Z80CPU::rst_28h       , 0, 11), /* e0 */
-/* f0 */ ("RET P"     , Z80CPU::ret_p      , 0, 5), ("POP AF"    , Z80CPU::pop_af     , 0, 10), ("JP P,"      , Z80CPU::jp_p_nn     , 2, 10), ("???"        , Z80CPU::invalid_opcode, 0,  4), ("CALL P,"   , Z80CPU::call_p_nn     , 2, 10), ("PUSH AF"   , Z80CPU::push_af       , 0, 11), ("OR"        , Z80CPU::or_n          , 1,  7), ("RST 30h"   , Z80CPU::rst_30h       , 0, 11), ("RET M"     , Z80CPU::ret_m         , 0,  5), ("LD SP, HL" , Z80CPU::ld_sp_hl      , 0,  6), ("JP M,"     , Z80CPU::jp_m_nn       , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0, 4), ("CALL M," , Z80CPU::call_m_nn     , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0,  4), ("CP"        , Z80CPU::cp_n          , 1, 7), ("RST 38h" , Z80CPU::rst_38h       , 0, 11)  /* f0 */
+/* f0 */ ("RET P"     , Z80CPU::ret_p      , 0, 5), ("POP AF"    , Z80CPU::pop_af     , 0, 10), ("JP P,"      , Z80CPU::jp_p_nn     , 2, 10), ("DI"         , Z80CPU::di            , 0,  4), ("CALL P,"   , Z80CPU::call_p_nn     , 2, 10), ("PUSH AF"   , Z80CPU::push_af       , 0, 11), ("OR"        , Z80CPU::or_n          , 1,  7), ("RST 30h"   , Z80CPU::rst_30h       , 0, 11), ("RET M"     , Z80CPU::ret_m         , 0,  5), ("LD SP, HL" , Z80CPU::ld_sp_hl      , 0,  6), ("JP M,"     , Z80CPU::jp_m_nn       , 2, 10), ("EI"      , Z80CPU::ei            , 0, 4), ("CALL M," , Z80CPU::call_m_nn     , 2, 10), ("???"     , Z80CPU::invalid_opcode, 0,  4), ("CP"        , Z80CPU::cp_n          , 1, 7), ("RST 38h" , Z80CPU::rst_38h       , 0, 11)  /* f0 */
 ];
 
 const IX_OPCODES: [(&str, fn(&mut Z80CPU, &mut dyn ReadWrite) -> u8, u8, u8); 256] = [
@@ -50,7 +50,8 @@ pub struct Z80CPU {
     iy: u16,
     sp: u16,
     pc: u16,
-    t_cycles: u8
+    t_cycles: u8,
+    interrupts_enabled: bool
 }
 
 pub struct Z80CPUState {
@@ -77,7 +78,8 @@ impl Z80CPU {
             iy: 0,
             sp: 0,
             pc: 0,
-            t_cycles: 0
+            t_cycles: 0,
+            interrupts_enabled: true
         }
     }
     
@@ -3803,6 +3805,11 @@ impl Z80CPU {
         0
     }
 
+    fn di(&mut self, _bus: &mut dyn ReadWrite) -> u8 {
+        self.interrupts_enabled = false;
+        0
+    }
+
     fn call_p_nn(&mut self, bus: &mut dyn ReadWrite) -> u8 {
         let n_low = bus.read(self.pc);
         self.pc = self.pc.wrapping_add(1);
@@ -3893,6 +3900,11 @@ impl Z80CPU {
             
             self.pc = (u16::from(n_high) << 8) + u16::from(n_low);
         }
+        0
+    }
+
+    fn ei(&mut self, _bus: &mut dyn ReadWrite) -> u8 {
+        self.interrupts_enabled = true;
         0
     }
 
@@ -9047,6 +9059,24 @@ mod tests {
     }
     
     #[test]
+    fn test_di() {
+        let mut cpu = Z80CPU::new();
+        let mut mock_bus = MockReadWrite::new();
+        mock_bus.expect_read().with(eq(0)).returning(|_| 0xf3);
+
+        cpu.reset();
+        let disasm = &cpu.get_next_instructions(&mock_bus, 1)[0];
+        cpu.t_cycles = 0;
+        cpu.interrupts_enabled = true;
+        cpu.clock(&mut mock_bus);
+        
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(cpu.interrupts_enabled, false);
+        assert_eq!(1 + cpu.t_cycles, 4);
+        assert_eq!(disasm, "0000: DI");
+    }
+
+    #[test]
     fn test_call_p_nn_when_s_is_0() {
         let mut cpu = Z80CPU::new();
         let mut mock_bus = MockReadWrite::new();
@@ -9261,6 +9291,24 @@ mod tests {
         assert_eq!(cpu.pc, 0x0003);
         assert_eq!(1 + cpu.t_cycles, 10);
         assert_eq!(disasm, "0000: JP M, BAADh");
+    }
+
+    #[test]
+    fn test_ei() {
+        let mut cpu = Z80CPU::new();
+        let mut mock_bus = MockReadWrite::new();
+        mock_bus.expect_read().with(eq(0)).returning(|_| 0xfb);
+
+        cpu.reset();
+        let disasm = &cpu.get_next_instructions(&mock_bus, 1)[0];
+        cpu.t_cycles = 0;
+        cpu.interrupts_enabled = false;
+        cpu.clock(&mut mock_bus);
+        
+        assert_eq!(cpu.pc, 0x0001);
+        assert_eq!(cpu.interrupts_enabled, true);
+        assert_eq!(1 + cpu.t_cycles, 4);
+        assert_eq!(disasm, "0000: EI");
     }
 
     #[test]
